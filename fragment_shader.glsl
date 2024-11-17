46cfb10bb6a4532ddbd0cd541765517616d8393a
deAssis Filho <barroso.assis.francisco@gmail.com>
2024-11-17 11:03:07 -0300

    Global ilumination instead of difuse ilumination

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -5,32 +5,36 @@ in vec2 TexCoord;
 
 out vec4 FragColor;
 
+uniform vec3 ambientColor;
+
 uniform sampler2D texture1;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-uniform vec3 lightColor;
+// uniform vec3 lightPos;
+// uniform vec3 viewPos;
+// uniform vec3 lightColor;
 
 void main()
 {
     // Ambient
-    float ambientStrength = 0.2;
-    vec3 ambient = ambientStrength * lightColor;
+    // float ambientStrength = 0.2;
+    // vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = ambientColor;
     
     // Diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);  
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    // vec3 norm = normalize(Normal);
+    // vec3 lightDir = normalize(lightPos - FragPos);  
+    // float diff = max(dot(norm, lightDir), 0.0);
+    // vec3 diffuse = diff * lightColor;
     
-    // Specular
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);  
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    // // Specular
+    // float specularStrength = 0.5;
+    // vec3 viewDir = normalize(viewPos - FragPos);  
+    // vec3 reflectDir = reflect(-lightDir, norm);  
+    // float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    // vec3 specular = specularStrength * spec * lightColor;  
     
-    // Combine results
-    vec3 lighting = (ambient + diffuse + specular);
+    // // Combine results
+    // vec3 lighting = (ambient + diffuse + specular);
     vec4 textureColor = texture(texture1, TexCoord);
-    FragColor = vec4(lighting, 1.0) * textureColor;
+    // FragColor = vec4(lighting, 1.0) * textureColor;
+    FragColor = vec4(ambient, 1.0) * textureColor;
 }
--- visualization.py
+++ visualization.py
@@ -1,7 +1,10 @@
 from OpenGL.GL import *
 import glm
 
-def lighting_setup(shader_program, position=glm.vec3(5, 5, 5), color=glm.vec3(1, 1, 1)):
-    # Lighting and camera positions
-    glUniform3fv(glGetUniformLocation(shader_program, 'lightPos'), 1, glm.value_ptr(position))
-    glUniform3fv(glGetUniformLocation(shader_program, 'lightColor'), 1, glm.value_ptr(color))
+# def lighting_setup(shader_program, position=glm.vec3(5, 5, 5), color=glm.vec3(1, 1, 1)):
+#     # Lighting and camera positions
+#     glUniform3fv(glGetUniformLocation(shader_program, 'lightPos'), 1, glm.value_ptr(position))
+#     glUniform3fv(glGetUniformLocation(shader_program, 'lightColor'), 1, glm.value_ptr(color))
+
+def lighting_setup(shader_program, ambient_color=(0.8, 0.8, 0.8)):
+    glUniform3fv(glGetUniformLocation(shader_program, 'ambientColor'), 1, ambient_color)
